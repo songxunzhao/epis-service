22a31cfdf768d8e9b6641cde05c31d94dbcadc3a
Jordan Valdma <jordan.valdma@gmail.com>
2017-09-03 18:16:54 +0300

    Updates acme challenge endpoint

--- src/main/java/ee/tuleva/epis/AcmeChallengeController.java
+++ src/main/java/ee/tuleva/epis/AcmeChallengeController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class AcmeChallengeController {
 
-	@RequestMapping(value = "/.well-known/acme-challenge/H9fmdF19meluzw4WOOOuJGyAzv0mNKDPq66aHNFkNwE")
+	@RequestMapping(value = "/.well-known/acme-challenge/FFdNE25rVBK_ZCy13dp2viktltzVs3FPCM-BCjQNUjU")
 	@ResponseBody
 	public String challenge() {
-		return "H9fmdF19meluzw4WOOOuJGyAzv0mNKDPq66aHNFkNwE.EMEBBxvSam3n_ien1J0z4dXeTuc2JuR3HqfAP6teLjE";
+		return "FFdNE25rVBK_ZCy13dp2viktltzVs3FPCM-BCjQNUjU.EMEBBxvSam3n_ien1J0z4dXeTuc2JuR3HqfAP6teLjE";
 	}
 
 }
