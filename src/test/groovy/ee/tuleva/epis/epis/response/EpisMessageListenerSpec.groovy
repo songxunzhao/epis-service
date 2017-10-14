1327c3fb077ee374c890c2213acaa57a4114b8a3
Jordan Valdma <jordan.valdma@gmail.com>
2017-10-14 17:40:31 +0300

    Adds Epis message listener test

--- src/test/groovy/ee/tuleva/epis/epis/response/EpisMessageListenerSpec.groovy
+++ src/test/groovy/ee/tuleva/epis/epis/response/EpisMessageListenerSpec.groovy
@@ -1,53 +1,46 @@
 package ee.tuleva.epis.epis.response
 
-import ee.tuleva.epis.epis.EpisMessageType
-import ee.tuleva.epis.epis.response.application.list.EpisApplicationListToMandateApplicationResponseListConverter
-import ee.tuleva.epis.mandate.processor.MandateProcess
-import ee.tuleva.epis.mandate.processor.MandateProcessRepository
-import ee.tuleva.epis.mandate.processor.MandateProcessResult
+import iso.std.iso._20022.tech.xsd.head_001_001.BusinessApplicationHeaderV01
+import mhub.xsd.envelope._01.Ex
+import org.springframework.jms.support.converter.MarshallingMessageConverter
+import org.springframework.jms.support.converter.MessageConverter
 import spock.lang.Specification
 
 import javax.jms.Message
+import javax.xml.bind.JAXBElement
 
 class EpisMessageListenerSpec extends Specification {
 
-    MandateProcessRepository mandateProcessRepository = Mock(MandateProcessRepository)
-    EpisMessageResponseHandler mandateMessageResponseHandler = Mock(EpisMessageResponseHandler)
     EpisMessageResponseStore episMessageResponseStore = Mock(EpisMessageResponseStore)
-    EpisApplicationListToMandateApplicationResponseListConverter applicationListConverter =
-            Mock(EpisApplicationListToMandateApplicationResponseListConverter)
+    MessageConverter messageConverter = Mock(MarshallingMessageConverter)
 
     EpisMessageListener service = new EpisMessageListener(
-            mandateProcessRepository,
-            mandateMessageResponseHandler,
             episMessageResponseStore,
-            applicationListConverter
+            messageConverter
     )
 
-    def "ProcessorListener: On message, persist it"() {
+    def "On message, store it"() {
         given:
-        String sampleProcessId = "123"
 
-        1 * mandateMessageResponseHandler.getMessageType(sampleMessage) >> EpisMessageType.APPLICATION_PROCESS
+        String sampleId = 'sampleId';
 
-        1 * mandateMessageResponseHandler.getMandateProcessResponse(sampleMessage) >>
-                MandateProcessResult.builder()
-                        .processId(sampleProcessId)
-                        .successful(true)
-                        .build()
-
-        1 * mandateProcessRepository.findOneByProcessId(sampleProcessId) >>
-                MandateProcess.builder()
-                        .processId(sampleProcessId)
-                        .build()
+        messageConverter.fromMessage(sampleMessage) >> Mock(Ex, {
+            getBizMsg() >> Mock(Ex.BizMsg, {
+                getAppHdr() >> Mock(BusinessApplicationHeaderV01, {
+                    getBizMsgIdr() >> sampleId
+                })
+                getAny() >> Mock(JAXBElement,{
+                    getValue() >> [:]
+                })
+            })
+        })
 
         when:
         service.processorListener().onMessage(sampleMessage)
         then:
-        1 * mandateProcessRepository.save({ MandateProcess process ->
-            process.processId == sampleProcessId && process.isSuccessful().get() == true
-        })
-
+        1 * episMessageResponseStore.storeOne(
+                sampleId, "{}"
+        );
     }
 
     Message sampleMessage = Mock(Message)
