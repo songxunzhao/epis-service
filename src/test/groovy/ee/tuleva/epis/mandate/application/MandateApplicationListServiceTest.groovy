92f6e1e40e7b4f36568b1207cc69506571e9d5d8
Erko Risthein <erko@risthein.ee>
2017-10-28 17:54:44 +0300

    Added MandateApplicationListService integration test

---
+++ src/test/groovy/ee/tuleva/epis/mandate/application/MandateApplicationListServiceTest.groovy
@@ -0,0 +1,25 @@
+package ee.tuleva.epis.mandate.application
+
+import org.junit.Ignore
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("dev")
+class MandateApplicationListServiceTest {
+
+  @Autowired
+  MandateApplicationListService service
+
+  @Ignore
+  @Test
+  public void testIt() {
+    String idCode = "38812022762"
+    service.get(idCode)
+  }
+}
