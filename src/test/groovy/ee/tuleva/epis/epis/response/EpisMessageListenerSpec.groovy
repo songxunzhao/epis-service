074802bd63b9ad2e223f120cda17f64a8289ed2b
Jordan Valdma <jordan.valdma@gmail.com>
2017-10-29 11:55:37 +0200

    Adds application process handling back

--- src/test/groovy/ee/tuleva/epis/epis/response/EpisMessageListenerSpec.groovy
+++ src/test/groovy/ee/tuleva/epis/epis/response/EpisMessageListenerSpec.groovy
@@ -1,6 +1,10 @@
 package ee.tuleva.epis.epis.response
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import ee.tuleva.epis.epis.EpisMessageType
+import ee.tuleva.epis.mandate.processor.MandateProcess
+import ee.tuleva.epis.mandate.processor.MandateProcessRepository
+import ee.tuleva.epis.mandate.processor.MandateProcessResult
 import iso.std.iso._20022.tech.xsd.head_001_001.BusinessApplicationHeaderV01
 import mhub.xsd.envelope._01.Ex
 import org.springframework.jms.support.converter.MarshallingMessageConverter
@@ -15,11 +19,15 @@ class EpisMessageListenerSpec extends Specification {
     EpisMessageResponseStore episMessageResponseStore = Mock(EpisMessageResponseStore)
     MessageConverter messageConverter = Mock(MarshallingMessageConverter)
     ObjectMapper objectMapper = new ObjectMapper()
+    MandateProcessRepository mandateProcessRepository = Mock(MandateProcessRepository)
+    EpisMessageResponseHandler mandateMessageResponseHandler = Mock(EpisMessageResponseHandler)
 
     EpisMessageListener service = new EpisMessageListener(
             episMessageResponseStore,
             messageConverter,
-            objectMapper
+            objectMapper,
+            mandateMessageResponseHandler,
+            mandateProcessRepository
     )
 
     def "On message, store it"() {
@@ -44,6 +52,34 @@ class EpisMessageListenerSpec extends Specification {
         1 * episMessageResponseStore.storeOne(
                 sampleId, "{}"
         );
+    }
+
+    //FIXME: this functionality needs to be removed
+    //and generalized to using response store
+    def "On application process message, persist it"() {
+        given:
+        String sampleProcessId = "123"
+
+        1 * mandateMessageResponseHandler.getMessageType(sampleMessage) >> EpisMessageType.APPLICATION_PROCESS
+
+        1 * mandateMessageResponseHandler.getMandateProcessResponse(sampleMessage) >>
+                MandateProcessResult.builder()
+                        .processId(sampleProcessId)
+                        .successful(true)
+                        .build()
+
+        1 * mandateProcessRepository.findOneByProcessId(sampleProcessId) >>
+                MandateProcess.builder()
+                        .processId(sampleProcessId)
+                        .build()
+
+        when:
+        service.processorListener().onMessage(sampleMessage)
+        then:
+        1 * mandateProcessRepository.save({ MandateProcess process ->
+            process.processId == sampleProcessId && process.isSuccessful().get() == true
+        })
+
     }
 
     Message sampleMessage = Mock(Message)
