c4084a3a08bb7c968bb2363773708f97bc20b9c2
Erko Risthein <erko@risthein.ee>
2017-11-12 17:20:20 +0200

    Fix account statement amounts

--- src/test/groovy/ee/tuleva/epis/account/EpisX14TypeToFundBalanceListConverterSpec.groovy
+++ src/test/groovy/ee/tuleva/epis/account/EpisX14TypeToFundBalanceListConverterSpec.groovy
@@ -8,12 +8,13 @@ class EpisX14TypeToFundBalanceListConverterSpec extends Specification {
 
     EpisX14TypeToFundBalanceListConverter converter = new EpisX14TypeToFundBalanceListConverter();
 
-    BigDecimal sampleAmount = new BigDecimal(1)
+    BigDecimal sampleAmount = new BigDecimal(100)
+    BigDecimal sampleNav = new BigDecimal("0.64")
     String sampleIsin1 = "sampleIsin1"
     String sampleIsin2 = "sampleIsin2"
     String sampleCurrency = "EUR"
 
-    def "Convert"() {
+    def "Converts EpisX14Type to List of Fund"() {
 
         when:
         List<FundBalance> response = converter.convert(getSampleSource())
@@ -22,22 +23,24 @@ class EpisX14TypeToFundBalanceListConverterSpec extends Specification {
         response.size() == 2
         response.first().isin == sampleIsin1
         response.first().currency == sampleCurrency
-        response.first().value == sampleAmount
+        response.first().value == sampleAmount.multiply(sampleNav)
         response.first().pillar == 2
         response.last().isin == sampleIsin2
         response.last().currency == sampleCurrency
-        response.last().value == sampleAmount
+        response.last().value == sampleAmount.multiply(sampleNav)
         response.last().pillar == 2
     }
 
     EpisX14Type getSampleSource() {
         EpisX14ResponseType.Unit sampleUnitThatWillRepeat = new EpisX14ResponseType.Unit()
         sampleUnitThatWillRepeat.setAmount(sampleAmount)
+        sampleUnitThatWillRepeat.setNAV(sampleNav)
         sampleUnitThatWillRepeat.setISIN(sampleIsin1)
         sampleUnitThatWillRepeat.setCurrency(sampleCurrency)
 
         EpisX14ResponseType.Unit sampleUnit = new EpisX14ResponseType.Unit()
         sampleUnit.setAmount(sampleAmount)
+        sampleUnit.setNAV(sampleNav)
         sampleUnit.setISIN(sampleIsin2)
         sampleUnit.setCurrency(sampleCurrency)
 
