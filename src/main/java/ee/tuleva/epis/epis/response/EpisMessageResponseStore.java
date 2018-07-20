181b13705939a64dc568da69e4f16934570d30ea
Rauno Viskus <Rauno56@gmail.com>
2018-07-20 10:22:39 +0300

    Remove PII from log output

--- src/main/java/ee/tuleva/epis/epis/response/EpisMessageResponseStore.java
+++ src/main/java/ee/tuleva/epis/epis/response/EpisMessageResponseStore.java
@@ -52,7 +52,7 @@ public class EpisMessageResponseStore {
         log.info("Waiting for mandate applications response at queue {}", queueName);
         // This is a blocking call
         String response = (String) amqpTemplate.receiveAndConvert(queueName, 60000);
-        log.info("Got response {}", response);
+        // log.info("Got response {}", response); // PII
 
         amqpAdmin.deleteQueue(queueName);
 
