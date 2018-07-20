fc57292a6050b928b60ccf7ba9662d8e39c973fa
Rauno Viskus <Rauno56@gmail.com>
2018-07-20 10:21:31 +0300

    Compare to absolute values for replacing zeros

--- src/main/java/ee/tuleva/epis/account/Transaction.java
+++ src/main/java/ee/tuleva/epis/account/Transaction.java
@@ -1,25 +1,24 @@
 package ee.tuleva.epis.account;
 
 import lombok.*;
-import lombok.extern.java.Log;
-import lombok.extern.slf4j.Slf4j;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 
 @Builder
 @Data
-@Slf4j
 public class Transaction {
   private Instant time;
   private BigDecimal amount;
   private String currency;
 
+  @Getter(AccessLevel.NONE)
   private final String DEFAULT_CURRENCY = "EUR";
+  @Getter(AccessLevel.NONE)
   private final BigDecimal MIN_AMOUNT = BigDecimal.valueOf(0.001);
 
   public Transaction replaceNulls() {
-    if (this.getAmount() == null || this.getAmount().compareTo(MIN_AMOUNT) < 0) {
+    if (this.getAmount() == null || this.getAmount().abs().compareTo(MIN_AMOUNT) < 0) {
       this.setAmount(BigDecimal.ZERO);
     }
     if (this.getCurrency() == null && this.getAmount().compareTo(BigDecimal.ZERO) == 0) {
