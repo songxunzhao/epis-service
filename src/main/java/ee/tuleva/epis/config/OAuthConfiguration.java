0ea41de0d77085bbc5f5d35ecbf73a35484f7c4e
Maido Käära <m@maido.io>
2019-04-30 11:25:15 +0300

    Exposing management endpoints with basic auth

--- src/main/java/ee/tuleva/epis/config/OAuthConfiguration.java
+++ src/main/java/ee/tuleva/epis/config/OAuthConfiguration.java
@@ -1,10 +1,13 @@
 package ee.tuleva.epis.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 public class OAuthConfiguration {
@@ -12,6 +15,13 @@ public class OAuthConfiguration {
     @Configuration
     @EnableResourceServer
     protected static class OAuthResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+        public static final String RESOURCE_REQUEST_MATCHER_BEAN = "resourceServerRequestMatcher";
+
+        @Bean(RESOURCE_REQUEST_MATCHER_BEAN)
+        public RequestMatcher resources() {
+            return new AntPathRequestMatcher("/v1/**");
+        }
 
         private static final String RESOURCE_ID = "onboarding-service";
 
@@ -23,9 +33,9 @@ public class OAuthConfiguration {
         @Override
         public void configure(HttpSecurity http) throws Exception {
             http
-                    .authorizeRequests()
-                    .regexMatchers("/v1/.*").authenticated()
-                    ;
+                .requestMatcher(resources()).authorizeRequests()
+                .anyRequest().authenticated()
+                .and().csrf().ignoringRequestMatchers(resources());
         }
     }
 }
--- src/main/java/ee/tuleva/epis/config/SecurityConfiguration.java
+++ src/main/java/ee/tuleva/epis/config/SecurityConfiguration.java
@@ -1,26 +1,39 @@
 package ee.tuleva.epis.config;
 
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import static ee.tuleva.epis.config.OAuthConfiguration.OAuthResourceServerConfig.RESOURCE_REQUEST_MATCHER_BEAN;
 
 @EnableWebSecurity
 @EnableOAuth2Client
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-			.requestMatchers(EndpointRequest.to("health")).permitAll()
-			.regexMatchers("/", "/swagger-ui.html", "/authenticate", "/idLogin", "/oauth/token")
-			.permitAll()
-			.anyRequest().authenticated()
-			.and()
-			.csrf().ignoringAntMatchers("/v1/**", "/authenticate", "/idLogin", "/oauth/token");
-	}
+    @Qualifier(RESOURCE_REQUEST_MATCHER_BEAN)
+    final RequestMatcher resources;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        val nonResources = new NegatedRequestMatcher(resources);
+        http.requestMatcher(nonResources)
+            .authorizeRequests()
+            .requestMatchers(EndpointRequest.to("health")).permitAll()
+            .requestMatchers(EndpointRequest.toAnyEndpoint().excluding("health")).authenticated()
+            .regexMatchers("/", "/swagger-ui.html")
+            .permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .httpBasic();
+    }
 }
