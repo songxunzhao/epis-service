0df48269aea96f9a7fe879200093ca5fd3365278
Maido Käära <m@maido.io>
2019-05-15 17:55:53 +0300

    Command and response

--- src/main/java/ee/tuleva/epis/mandate/MandateCommand.java
+++ src/main/java/ee/tuleva/epis/mandate/MandateCommand.java
@@ -1,7 +1,35 @@
 package ee.tuleva.epis.mandate;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+
+@Getter
+@Setter
 public class MandateCommand {
+    @NotNull
+    private Long id;
+
+    @NotNull
+    private String futureContributionFundIsin;
+
+    @NotNull
+    private Instant createdDate;
+
+    private Integer pillar = 2;
+
+    List<FundTransferExchange> fundTransferExchanges;
+
+    @Getter
+    @Setter
+    public static class FundTransferExchange {
+        private Long id;
+        private String sourceFundIsin;
+        private BigDecimal amount;
+        private String targetFundIsin;
+    }
 }
--- src/main/java/ee/tuleva/epis/mandate/MandateResponse.java
+++ src/main/java/ee/tuleva/epis/mandate/MandateResponse.java
@@ -1,7 +1,11 @@
 package ee.tuleva.epis.mandate;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class MandateResponse {
+    private boolean successful;
+    private Integer errorCode;
 }
