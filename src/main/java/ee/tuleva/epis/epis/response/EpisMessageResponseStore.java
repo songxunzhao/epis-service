d163999ec74f3efb8a7dd1b33c677a7d6c153d49
Maido Käära <m@maido.io>
2019-06-01 13:03:04 +0300

    Reduce epis timeout to 10s

--- src/main/java/ee/tuleva/epis/epis/response/EpisMessageResponseStore.java
+++ src/main/java/ee/tuleva/epis/epis/response/EpisMessageResponseStore.java
@@ -51,7 +51,7 @@ public class EpisMessageResponseStore {
 
         log.info("Waiting for mandate applications response at queue {}", queueName);
         // This is a blocking call
-        String response = (String) amqpTemplate.receiveAndConvert(queueName, 60_000);
+        String response = (String) amqpTemplate.receiveAndConvert(queueName, 10_000);
         log.info("Got response {}", response);
 
         amqpAdmin.deleteQueue(queueName);
