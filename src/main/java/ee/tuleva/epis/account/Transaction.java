61b4ca21abcb13a72402f94a7333e3d9a9d1d199
Erko Risthein <erko@risthein.ee>
2020-05-28 17:04:40 +0300

    Add SUBTRACTION transaction types

--- src/main/java/ee/tuleva/epis/account/Transaction.java
+++ src/main/java/ee/tuleva/epis/account/Transaction.java
@@ -7,6 +7,7 @@ import org.springframework.lang.NonNull;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Objects;
 
 @Builder
 @Data
@@ -19,7 +20,7 @@ public class Transaction implements Comparable<Transaction> {
     private final Type type;
 
     public enum Type {
-        CONTRIBUTION, OTHER;
+        CONTRIBUTION_CASH, CONTRIBUTION, SUBTRACTION, OTHER;
 
         @NonNull
         public static Type from(Integer purposeCode) {
@@ -27,7 +28,27 @@ public class Transaction implements Comparable<Transaction> {
             // 41 = Osakute väljalase tööandjalt laekumiste alusel (3. sammas)
             // 42 = Osakute väljalase isikult laekumiste alusel (3. sammas)
             if (Arrays.asList(1, 41, 42).contains(purposeCode)) {
+                return CONTRIBUTION_CASH;
+            }
+            // 43 = Osakute väljalase kindlustuslepingust laekumiste alusel (3. sammas)
+            if (Objects.equals(43, purposeCode)) {
                 return CONTRIBUTION;
+            }
+            // 6 = Kindlustuslepingu sõlmimine (2. sammas)
+            // 8 = Väljamaksed pensionifondist (2. sammas)
+            // 9 = Täiendavad väljamaksed pensionifondist (2. sammas)
+            // 10 = Pärimine vana (2. ja 3. sammas)
+            // 16 = Täiendavad maksed kindlustusseltsi (2. sammas)
+            // 18 = Pärandvara pankrott (2. sammas)
+            // 19 = Ülekanne EU institutsioonide pensioniskeemi (2. sammas)
+            // 21 = Osakute kustutamine - näiteks pärijate puudumisel peale 10 aastat (2. sammas)
+            // 51 = Lunastamine (3. sammas)
+            // 52 = Kohtutäituri lunastus  (3. sammas)
+            // 53 = Kindlustusse ülekanne (3. sammas)
+            // 55 = Pärimine uus (2. ja 3. sammas)
+            // 56 = Ühisvara jagamine (3. sammas)
+            if (Arrays.asList(6, 8, 9, 10, 16, 18, 19, 21, 51, 52, 53, 55, 56).contains(purposeCode)) {
+                return SUBTRACTION;
             }
             return OTHER;
         }
